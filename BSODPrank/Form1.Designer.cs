79188a22cf4a3490ee24d904143fef7de604c1ba
Alee <Alee14498@gmail.com>
2019-02-10 10:50:16 -0500

    Added the bsod picture and added a debug exit button

--- BSODPrank/Form1.Designer.cs
+++ BSODPrank/Form1.Designer.cs
@@ -28,22 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExitDebug = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImage = global::BSODPrank.Properties.Resources.bsod;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(823, 516);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnExitDebug
+            // 
+            this.btnExitDebug.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExitDebug.BackColor = System.Drawing.Color.White;
+            this.btnExitDebug.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExitDebug.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExitDebug.Location = new System.Drawing.Point(687, 456);
+            this.btnExitDebug.Name = "btnExitDebug";
+            this.btnExitDebug.Size = new System.Drawing.Size(92, 37);
+            this.btnExitDebug.TabIndex = 1;
+            this.btnExitDebug.Text = "exit";
+            this.btnExitDebug.UseVisualStyleBackColor = false;
+            this.btnExitDebug.Visible = false;
+            this.btnExitDebug.Click += new System.EventHandler(this.btnExitDebug_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(813, 516);
+            this.Controls.Add(this.btnExitDebug);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "BSOD";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnExitDebug;
     }
 }
 
--- BSODPrank/Form1.cs
+++ BSODPrank/Form1.cs
@@ -19,12 +19,23 @@ namespace BSODPrank
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            #if DEBUG
+            MessageBox.Show("Debug mode is on.");
+            btnExitDebug.Visible = true;
+            #endif
+            //System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            //player.SoundLocation = "Bleep-SoundBible.com-1927126940.wav";
+            //player.Play();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
 
+        }
+
+        private void btnExitDebug_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
